3f5f5d9da7c73a42773962b3cd71e7a1d85c626e
ketul-andev <ketul@adnabu.com>
2025-06-14 20:00:45 +0530

    feat: Integrate identify app into project with URL routing and identify view

--- identify/views.py
+++ identify/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+import json
+from .models import Contact
+from django.db import transaction
+from django.db.models import Q
 
-# Create your views here.
+@csrf_exempt
+@require_POST
+def identify(request):
+    data = json.loads(request.body)
+    print(data)
+    return JsonResponse({"status": "success"})
