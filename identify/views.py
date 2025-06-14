634d8f7fa4069e55318f94a2be5967ab102f3f8b
ketul-andev <ketul@adnabu.com>
2025-06-14 23:46:56 +0530

    feat: added invalid request body check and improved logic to handle primary to secondary conversion

--- identify/views.py
+++ identify/views.py
@@ -2,35 +2,31 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 import json
-from .functions import get_primary_contacts, create_contact, get_consolidated_contact, check_if_contact_exists, update_primary_contact
-from .serializers import ContactSerializer
+from .functions import get_all_contacts, create_contact, get_consolidated_contact, check_if_contact_exists, update_primary_contact, check_if_new_info
 
 @csrf_exempt
 @require_POST
 def identify(request):
     data = json.loads(request.body)
+    if not data or not isinstance(data, dict):
+        return JsonResponse({"status": "ERR", "message": "Invalid request body"}, status=400)
     email = data.get('email')
     phoneNumber = data.get('phoneNumber')
     if not email and not phoneNumber:
         return JsonResponse({"status": "ERR", "message": "Email or phoneNumber is required"}, status=400)
     
-    priamry_contacts = get_primary_contacts(email, phoneNumber)
-    if priamry_contacts.count() > 1:
-        print("More than 1 primary contact")
-        update_primary_contact(priamry_contacts[1], priamry_contacts[0])
-        return JsonResponse({"contact": get_consolidated_contact(priamry_contacts[0])})
-    elif priamry_contacts.count() == 1:
-        print("1 primary contact")
-        if check_if_contact_exists(email, phoneNumber):
-            return JsonResponse({"contact": get_consolidated_contact(priamry_contacts[0])})
-        else:
-            if email and phoneNumber:
-                new_contact = create_contact(email, phoneNumber, 'secondary', priamry_contacts[0])
-                return JsonResponse({"contact": get_consolidated_contact(new_contact)})
-            else:
-                return JsonResponse({"contact": get_consolidated_contact(priamry_contacts[0])})
-    else:
-        print("No primary contact")
+    all_contacts = get_all_contacts(email, phoneNumber)
+    if all_contacts.count() == 0:
         new_contact = create_contact(email, phoneNumber, 'primary')
         return JsonResponse({"contact": get_consolidated_contact(new_contact)})
+    else:
+        primary_contact = all_contacts.first()
+        for contact in all_contacts:
+            if contact.id != primary_contact.id and contact.linkPrecedence != 'secondary':
+                update_primary_contact(contact, primary_contact)
+        if check_if_new_info(email, phoneNumber):
+            new_contact = create_contact(email, phoneNumber, 'secondary', primary_contact)
+            return JsonResponse({"contact": get_consolidated_contact(new_contact)})
+        return JsonResponse({"contact": get_consolidated_contact(primary_contact)})
+
     
